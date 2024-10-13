744e7c32df6ca2e7942fdbd83d41af8396a00172
arigree <arigree@iu.edu>
2024-10-12 21:48:21 -0400

    Added ability to add items to cart on cart.html

--- app/app.js
+++ app/app.js
@@ -1,70 +1,86 @@
 // import { changePage, checkLogin } from "../model/model.js";
-import { changePage, checkLogin, register } from "../model/model.js";
-
+import { changePage, checkLogin, register, loadCart, addToCart } from "../model/model.js";
 
 function route() {
-    let hashTag = window.location.hash;
-    let pageID = hashTag.replace("#", "");
-    changePage(pageID);
-    //console.log("route " + pageID);
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  changePage(pageID);
+  //console.log("route " + pageID);
 }
 
 function initSite() {
-    $(window).on("hashchange", route);
-    route();
-    // $("#loginBtn").on("click", function() {checkLogin()});
-    // $("#loginButton").on("click", function(){
-    //     const email = $("#loginEmail").val();
-    //     const password = $("#loginPassword").val();
-    //     checkLogin(email, password);
-    //     // console.log(email);
-    // });
-    // $("#signupButton").on("click", function(){
-    //     const firstName = $("#firstName").val();
-    //     const lastName = $("#lastName").val();
-    //     const email = $("#email").val();
-    //     const password = $("#password").val();
-    //     //if there is a better way to do this i dont want to know
-    //     register(firstName, lastName, email, password);
-    // console.log(firstName)
-    // });
+  $(window).on("hashchange", route);
+  route();
+  // $("#loginBtn").on("click", function() {checkLogin()});
+  // $("#loginButton").on("click", function(){
+  //     const email = $("#loginEmail").val();
+  //     const password = $("#loginPassword").val();
+  //     checkLogin(email, password);
+  //     // console.log(email);
+  // });
+  // $("#signupButton").on("click", function(){
+  //     const firstName = $("#firstName").val();
+  //     const lastName = $("#lastName").val();
+  //     const email = $("#email").val();
+  //     const password = $("#password").val();
+  //     //if there is a better way to do this i dont want to know
+  //     register(firstName, lastName, email, password);
+  // console.log(firstName)
+  // });
 }
 
-
 $(document).ready(function () {
-    initSite();
-    
-    $(document).on("click", "#loginButton", function(event) {
-        event.preventDefault();
-        console.log("login");
-        
-        const email = $("#loginEmail").val();
-        const password = $("#loginPassword").val();
-        
-        console.log("email:", email, "password:", password);
-        
-        if (checkLogin(email, password)) {
-            console.log("login worked");
-            window.location.hash = "confirmation";
-        } else {
-            console.log("login no work");
-        }
-    });
-    
-    $(document).on("click", "#signupButton", function(event) {
-        event.preventDefault();
-        console.log("signup");
-        
-        const firstName = $("#firstName").val();
-        const lastName = $("#lastName").val();
-        const email = $("#signupEmail").val();
-        const password = $("#signupPassword").val();
-        
-        if (register(firstName, lastName, email, password)) {
-            console.log("signup work");
-            window.location.hash = "confirmation";
-        } else {
-            console.log("signup no work");
-        }
-    });
+  initSite();
+
+  $(document).on("click", "#loginButton", function (event) {
+    event.preventDefault();
+    console.log("login");
+
+    const email = $("#loginEmail").val();
+    const password = $("#loginPassword").val();
+
+    console.log("email:", email, "password:", password);
+
+    if (checkLogin(email, password)) {
+      console.log("login worked");
+      window.location.hash = "confirmation";
+    } else {
+      console.log("login no work");
+    }
+  });
+
+  $(document).on("click", "#signupButton", function (event) {
+    event.preventDefault();
+    console.log("signup");
+
+    const firstName = $("#firstName").val();
+    const lastName = $("#lastName").val();
+    const email = $("#signupEmail").val();
+    const password = $("#signupPassword").val();
+
+    if (register(firstName, lastName, email, password)) {
+      console.log("signup work");
+      window.location.hash = "confirmation";
+    } else {
+      console.log("signup no work");
+    }
+  });
+
+  $(document).on("click", ".button", function (event) {
+    event.preventDefault();
+    const bookElement = $(this).closest(".book");
+    const cover = bookElement.find("img").attr("src");
+    const price = bookElement.find("h4").text();
+    const description = bookElement.find("p").text();
+
+    const cartItem = {
+      cover,
+      price,
+      description,
+    };
+    addToCart(cartItem);
+
+    console.log("in cart maybe?", cartItem);
+    window.location.hash = "cart";
+  });
 });
--- model/model.js
+++ model/model.js
@@ -1,4 +1,5 @@
 export function changePage(pageName) {
+    
     console.log(pageName);
     // if (pageName != "") {
     //     $.get("pages/" + pageName + ".html", (data) =>{
@@ -16,6 +17,9 @@ export function changePage(pageName) {
     if (pageName) {
         $.get("pages/" + pageName + ".html", (data) =>{
             $("#app").html(data);
+            if(pageName === "cart"){
+                loadCart();
+            }
         }).fail((error) => {
             console.log("error " + error);
             alert("error " + error);
@@ -44,6 +48,43 @@ export function register(firstName, lastName, email, password) {
         return false;
     }
     return true;
+}
+
+let cartItems = [];
+
+export function addToCart(item){
+    cartItems.push(item);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+}
+
+export function loadCartItems(){
+    const items = localStorage.getItem("cartItems");
+    return items ? JSON.parse(items):[];
+}
+
+export function loadCart(){
+    console.log("loading?")
+    const cartItems = loadCartItems();
+    console.log("load items", cartItems)
+    const cartContainer = $("#cartItems");
+
+    cartContainer.empty();
+    if (cartItems.length === 0){
+        cartContainer.html("<p>Cart Empty:(</p>")
+        return
+    } //dont really need this just messing around with it, if causes issues delete
+
+    cartItems.forEach(item => {
+        const cartItemHTML = `
+        <div class="cartItem">
+        <img src="${item.cover}" alt="Book Cover"/>
+        </div>
+        <div>
+        <p>${item.description}</p>
+        <h4>${item.price}</h4>
+        </div>`;
+        cartContainer.append(cartItemHTML);
+    });
 }
 
 // export function checkLogin() {
